5547cd1ab2fbf9dfd82b53c0b2e587503597ab69
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-05-22 11:00:14 -0300

    RF001: implementando a model Employee

---
+++ model/employee.py
@@ -0,0 +1,13 @@
+class Employee:
+
+    #TODO: apply encapsulation by converting attributes to private or protected instead of public
+
+    def __init__(self, name, cpf, role, username, password_hash):
+        self.name = name
+        self.cpf = cpf
+        self.role = role
+        self.username = username
+        self.password_hash = password_hash
+
+    def __str__(self):
+        return f'Name: {self.name}, CPF: {self.cpf}, Role: {self.role}, Login: {self.username}'
