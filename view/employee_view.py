ec87d47ae86e745f04ec71a37f5965f0033449b6
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-05-28 08:34:21 -0300

    RF001: capturando dados do funcionario

--- view/employee_view.py
+++ view/employee_view.py
@@ -1,11 +1,17 @@
 def show_menu():
-    print('\n=== Library Management System ===') # Sistema Gerenciador de Biblioteca
-    print('1. Register Employee') # Registrar Funcionário
-    print('2. List Employees') # Listar Funcionários
-    print('3. Authenticate Employee') # Autenticar Funcionário
-    print('0. Exit') # Sair
+    print('\n=== Library Management System ===')  # Sistema Gerenciador de Biblioteca
+    print('1. Register Employee')  # Registrar Funcionário
+    print('2. List Employees')  # Listar Funcionários
+    print('3. Authenticate Employee')  # Autenticar Funcionário
+    print('0. Exit')  # Sair
 
-#TODO: def get_employee_data():
 
-#TODO: def get_auth_data():
+def get_employee_data():
+    name = input("Name: ")
+    cpf = input("CPF: ")
+    role = input("Role: ")
+    username = input("Username: ")
+    password = input("Password: ")
+    return name, cpf, role, username, password
 
+# TODO: def get_auth_data():
