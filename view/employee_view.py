60645b46ed1513ecfcad86762662c5874a18e59b
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-05-28 08:32:40 -0300

    RF001: exibindo menu

---
+++ view/employee_view.py
@@ -0,0 +1,11 @@
+def show_menu():
+    print('\n=== Library Management System ===') # Sistema Gerenciador de Biblioteca
+    print('1. Register Employee') # Registrar Funcionário
+    print('2. List Employees') # Listar Funcionários
+    print('3. Authenticate Employee') # Autenticar Funcionário
+    print('0. Exit') # Sair
+
+#TODO: def get_employee_data():
+
+#TODO: def get_auth_data():
+
