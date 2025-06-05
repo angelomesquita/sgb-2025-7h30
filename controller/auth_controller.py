976df63990cc77376ebfb429cd042f3e99b60dab
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-06-05 09:55:49 -0300

    RF001: incluindo typing em auth_controller

--- controller/auth_controller.py
+++ controller/auth_controller.py
@@ -4,7 +4,7 @@ from model.auth import Auth
 class AuthController:
 
     @staticmethod
-    def auth(employees, username, password):
+    def auth(employees: list, username: str, password: str) -> bool:
         for employee in employees:
             if Auth.auth(employee, username, password) and employee.deleted is not True:
                 print(f"Welcome, {employee.name}")
