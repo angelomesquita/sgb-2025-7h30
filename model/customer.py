4609cad7dceb3ee16424d8269a8fddab35a15855
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-06-11 08:44:57 -0300

    RF002: criando a model Customer

---
+++ model/customer.py
@@ -0,0 +1,14 @@
+class Customer:
+
+    # TODO: apply encapsulation by converting attributes to private or protected instead of public
+
+    def __init__(self, name: str, cpf: str, contact: str, category: str, password_hash: str):
+        # TODO Is customer equals to Employee?
+        self.name = name
+        self.cpf = cpf
+        self.contact = contact
+        self.category = category  # student, teacher, visitor
+        self.password_hash = password_hash
+
+    def __str__(self):
+        return f'Name: {self.name}, CPF: {self.cpf}, Contact: {self.contact}, Category: {self.category}'
