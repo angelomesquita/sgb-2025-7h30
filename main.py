30556be609fdc3e3ef812f9dc0c08b748cfc43fe
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-06-12 10:56:36 -0300

    usando dicionario para as opcoes do menu principal

--- main.py
+++ main.py
@@ -2,34 +2,54 @@ from view.employee_view import EmployeeView
 
 
 def main():
+    show_app_menu()
 
-    while True:
-        show_app_menu()
+
+def option_1() -> None:
+    employee_view = EmployeeView()
+    employee_view.show_menu()
+    EmployeeView.clear_screen()
+
+
+def option_2() -> None:
+    # custormer_view = CustomerView()
+    #custormer_view.show_menu()
+    #CustomerView.clear_screen()
+    print('Menu do módulo de usuário')
+
+
+def option_0() -> False:
+    print('Exiting the system.')  # saindo do sistema
+    return False
+
+
+def invalid_option() -> True:
+    print('Invalid option.')  # Opção Inválida
+    EmployeeView.press_enter_to_continue()
+    return True  # continuar no loop
+
+
+# TODO: Lição 13 Módulo de coleções: listas, tuplas, conjuntos e dicionários
+menu_actions = {
+    '1': option_1,
+    '2': option_2,
+    '0': option_0
+}
+
+
+def show_app_menu() -> None:
+    running = True
+    while running:
+        EmployeeView.clear_screen()
+        print('\n=== Library Management System ===')  # Sistema Gerenciador de Bibliotecas
+        print('1. Employee Module')  # Módulo Funcionários
+        print('2. Customer Module')  # Módulo Usuário (Estudante, Professor ou Visitante)
+        print('0. Exit')  # Sair do Sistema
 
         option = input('Select an option: ')
-
-        if option == '1':
-            employee_view = EmployeeView()
-            employee_view.show_menu()
-            EmployeeView.clear_screen()
-        elif option == '2':
-            #custormer_view = CustomerView()
-            #custormer_view.show_menu()
-            #CustomerView.clear_screen()
-            print('Menu do módulo de usuário')
-        elif option == '0':
-            print('Exiting the system.') # Saindo do Sistema
-            break
-        else:
-            print('Invalid option.') # Opção Inválida
-            EmployeeView.press_enter_to_continue()
-
-
-def show_app_menu():
-    print('\n=== Library Management System ===')  # Sistema Gerenciador de Bibliotecas
-    print('1. Employee Module')  # Módulo Funcionários
-    print('2. Customer Module')  # Módulo Usuário (Estudante, Professor ou Visitante)
-    print('0. Exit')  # Sair do Sistema
+        action = menu_actions.get(option, invalid_option)
+        result = action()
+        running = result if result is not None else True
 
 
 if __name__ == '__main__':
