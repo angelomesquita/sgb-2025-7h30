cb3f2a8e59f7e90286c230efa6a9c83533a90bec
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-08-13 08:09:14 -0300

    WIP

--- model/auth.py
+++ model/auth.py
@@ -5,13 +5,15 @@ from model.employee import Employee
 class Auth:
 
     @staticmethod
-    def hash_password(password: str) -> str:
+    def hash_password(password: str) -> bytes:
         password_bytes = password.encode('utf-8')
-        return str(bcrypt.hashpw(password_bytes, bcrypt.gensalt()))
+        return bcrypt.hashpw(password_bytes, bcrypt.gensalt())
 
     @staticmethod
     def verify_password(password: str, password_hash: str) -> bool:
-        return bcrypt.checkpw(password.encode('utf-8'), bytes(password_hash))
+        if isinstance(password_hash, str):
+            password_hash = password_hash.encode('utf-8')
+        return bcrypt.checkpw(password.encode('utf-8'), password_hash)
 
     @staticmethod
     def auth(employee: Employee, username: str, password: str) -> bool:
