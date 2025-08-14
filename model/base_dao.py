97671cf030e8ec567d57026dcf2139984b46100c
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-08-14 10:25:40 -0300

    add documentação em base_dao

--- model/base_dao.py
+++ model/base_dao.py
@@ -3,7 +3,42 @@ import os
 from abc import ABC, abstractmethod
 from typing import Generic, List, TypeVar
 
-T = TypeVar("T")  # Generic Type (Customer, Employee, etc...)
+T = TypeVar("T")
+
+"""
+BaseDao is an abstract generic class that provides common persistence operations
+for saving and loading objects from a text file.
+
+This class serves as a base for specific DAO (Data Access Object) implementations,
+such as EmployeeDao and CustomerDao, avoiding code duplication by centralizing
+the file handling logic.
+
+Type Parameters:
+    T: The type of the object handled by the DAO (e.g, Customer, Employee)
+    
+Class Attributes:
+    _FILE_PATH (str): Path to the file where the data will be stored.
+                      This must be define in subclasses.
+                      
+Class Methods:
+    save_all(items: List[T]) -> None:
+        Saves all objects from the provided list into the file defined by `_FILE_PATH`.
+        Each object is converted to a string representation using the `_serialize` method.
+        
+    load_all() -> List[T]:
+        Load all objects from the file defined by `_FILE_PATH` and returns them as a list.
+        Each line in the file is converted back to an object using the `_deserialize` method.
+        If the file does not exist, it returns an empty list.
+        
+Abstract Methods:
+     _serialize(item: T) -> str:
+        Converts an object into a string for saving to the file.
+        Must be implemented by the subclass to define the serialization format.
+        
+     _deserialize(data: str) -> T:
+        Converts a string read from the file back int an object
+        Must be implemented by the subclass to define the deserialization format.
+"""
 
 
 class BaseDao(ABC, Generic[T]):
@@ -31,11 +66,9 @@ class BaseDao(ABC, Generic[T]):
     @staticmethod
     @abstractmethod
     def _serialize(item: T) -> str:
-        """Converts the object into a string to save to the file"""
         pass
 
     @staticmethod
     @abstractmethod
     def _deserialize(data: str) -> T:
-        """Converts the string read from the file an object"""
         pass
