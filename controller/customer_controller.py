9155e7236c2f11c5370b6426e0aa57a78d3761fe
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-08-28 09:59:43 -0300

    add herança em customer_controller

--- controller/customer_controller.py
+++ controller/customer_controller.py
@@ -1,88 +1,23 @@
-from typing import Optional
-
-from model.auth import Auth
+from controller.base_controller import BaseController
 from model.category import Category
 from model.customer import Customer
 from model.customer_dao import CustomerDao
+from model.logger import customer_logger
 
 
-class CustomerController:
-    def __init__(self):
-        self.customers = CustomerDao.load_all()
+class CustomerController(BaseController[Customer]):
+
+    dao_class = CustomerDao
+    logger = customer_logger
 
     def register(self, name: str, cpf: str, contact: str, category: str, password: str) -> None:
-        if self.find(cpf):
-            print('An Customer with this CPF is already registered!\n')
-            return
-        if self.find_deleted(cpf):
-            print('An Customer with this CPF was previously deleted.\n')
-            return
         if not Category.validate(category):
             print('Invalid category. Try again.')
             return
-        password_hash = Auth.hash_password(password)
-        customer = Customer(name, cpf, contact, category, str(password_hash))
-        self.customers.append(customer)
-        CustomerDao.save_all(self.customers)
-        print('✅ Customer successfully registered!')
+        super().register(cpf, name=name, contact=contact, category=category, password=password)
 
-    def list(self) -> None:
-        if not self.customers:
-            print("No customers registered yet.")
-            return
-        actives_customers = [customer for customer in self.customers if not getattr(customer, 'deleted', False)]
-        if not actives_customers:
-            print("No active customers found.")
-            return
-        for customer in actives_customers:
-            print(customer)
-
-    def find(self, cpf: str) -> Optional[Customer]:
-        for customer in self.customers:
-            if customer.cpf == cpf and customer.deleted is not True:
-                return customer
-        return None
-
-    def find_deleted(self, cpf: str) -> Optional[Customer]:
-        for customer in self.customers:
-            if customer.cpf == cpf and getattr(customer, 'deleted', False) is True:
-                return customer
-        return None
+    def create_instance(self, name: str, cpf: str, contact: str, category: str, password_hash: str, deleted: bool = False) -> Customer:
+        return Customer(name, cpf, contact, category, password_hash, deleted)
 
     def update(self, name: str, cpf: str, contact: str, category: str, password: str) -> None:
-        for customer in self.customers:
-            if customer.cpf == cpf and customer.deleted is not True:
-                if name is not None:
-                    customer.name = name
-                if contact is not None:
-                    customer.contact = contact
-                if category is not None:
-                    customer.category = category
-                if password is not None:
-                    customer.password_hash = Auth.hash_password(password)
-                CustomerDao.save_all(self.customers)
-                print('Customer successfully updated!\n')
-                return
-            self.__customer_not_found()
-
-    def delete(self, cpf: str) -> None:
-        for customer in self.customers:
-            if customer.cpf == cpf and customer.deleted is not True:
-                customer.deleted = True
-                CustomerDao.save_all(self.customers)
-                print('Customer successfully deleted!\n')
-                return
-        self.__customer_not_found()
-
-    def restore(self, cpf: str) -> None:
-        for customer in self.customers:
-            if customer.cpf == cpf and customer.deleted is True:
-                customer.deleted = False
-                CustomerDao.save_all(self.customers)
-                print('Customer successfully restored!\n')
-                return
-        self.__customer_not_found()
-
-    @staticmethod
-    def __customer_not_found() -> None:
-        print('Customer not found.\n')
+        super().update(cpf, name=name, contact=contact, category=category, password=password)
