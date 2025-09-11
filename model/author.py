3a7ca0eea0a9ce08ed7d077e5937224d2cc64c05
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-09-11 10:56:46 -0300

    add atributo author_id

--- model/author.py
+++ model/author.py
@@ -1,8 +1,17 @@
 class Author:
 
-    def __init__(self, name: str, deleted: bool = False):
+    def __init__(self, author_id: int, name: str, deleted: bool = False):
+        self._author_id = author_id
         self._name = name
         self._deleted = deleted
+
+    @property
+    def author_id(self) -> int:
+        return self._author_id
+
+    @author_id.setter
+    def author_id(self, value: int) -> None:
+        self._author_id = value
 
     @property
     def name(self) -> str:
@@ -13,4 +22,4 @@ class Author:
         self._name = value
 
     def __str__(self):
-        return f"Name: {self.name}"
+        return f"ID: {self.author_id} - Name: {self.name}"
