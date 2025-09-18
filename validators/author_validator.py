5c758c5c8d62246031a2498ec6dd53818bda2cbe
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-09-18 09:34:50 -0300

    add validação de author_id no validator

--- validators/author_validator.py
+++ validators/author_validator.py
@@ -4,5 +4,9 @@ from validators.validator import Validator
 class AuthorValidator:
 
     @staticmethod
+    def validate_author_id(author_id: str) -> bool:
+        return Validator.is_numeric(author_id)
+
+    @staticmethod
     def validate_name(name: str) -> bool:
-        return Validator.min_length(name, 15)
+        return Validator.min_length(name, 3)
