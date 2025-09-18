5c2f4b1612d257a48001149c8c01d103bd089323
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-09-18 09:52:19 -0300

    add publisher_dao

---
+++ model/publisher_dao.py
@@ -0,0 +1,19 @@
+from model.base_dao import BaseDao
+from model.publisher import Publisher
+
+
+class AuthorDao(BaseDao[Publisher]):
+
+    _FILE_PATH = 'publishers.txt'
+
+    @staticmethod
+    def _serialize(p: Publisher) -> str:
+        return f"{p.publisher_id}|{p.legal_name}|{p.city}|{p.state}|{p.deleted}"
+
+    @staticmethod
+    def _deserialize(data: str) -> Publisher:
+        publisher_id, legal_name, city, state, deleted = data.split("|")
+        publisher = Publisher(publisher_id, legal_name, city, state, deleted)
+        publisher.deleted = deleted.lower() == "true"
+
+        return publisher
