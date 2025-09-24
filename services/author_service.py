18267ef8c3d3f1cf8ecf1c49667256d08bfc8124
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-09-24 09:08:40 -0300

    add services para buscar autores e editoras

--- services/author_service.py
+++ services/author_service.py
@@ -1,3 +1,4 @@
+from typing import List, Tuple
 from model.author import Author
 from model.author_dao import AuthorDao
 from model.exceptions import AuthorNotFoundError
@@ -6,9 +7,19 @@ from model.exceptions import AuthorNotFoundError
 class AuthorService:
 
     @staticmethod
+    def _get_all_authors() -> List[Author]:
+        """Load all authors from DAO (active and deleted)"""
+        return AuthorDao.load_all()
+
+    @staticmethod
     def get_author_by_id(author_id: str) -> Author:
-        authors = AuthorDao.load_all()
+        authors = AuthorService._get_all_authors()
         author = next((a for a in authors if str(a.author_id) == author_id), None)
         if author is None:
             raise AuthorNotFoundError(f"Author with id {author_id} not found.")
         return author
+
+    @staticmethod
+    def options() -> List[Tuple[str, str]]:
+        authors = AuthorService._get_all_authors()
+        return [(str(a.author_id), a.name) for a in authors if not a.deleted]
--- services/publisher_service.py
+++ services/publisher_service.py
@@ -1,3 +1,4 @@
+from typing import List, Tuple
 from model.publisher import Publisher
 from model.publisher_dao import PublisherDao
 from model.exceptions import PublisherNotFoundError
@@ -6,9 +7,19 @@ from model.exceptions import PublisherNotFoundError
 class PublisherService:
 
     @staticmethod
+    def _get_all_publishers() -> List[Publisher]:
+        """Load all publishers from DAO (active and deleted)"""
+        return PublisherDao.load_all()
+
+    @staticmethod
     def get_publisher_by_id(publisher_id: str) -> Publisher:
-        publishers = PublisherDao.load_all()
+        publishers = PublisherService._get_all_publishers()
         publisher = next((p for p in publishers if str(p.publisher_id) == publisher_id), None)
         if publisher is None:
             raise PublisherNotFoundError(f"Publisher with id {publisher_id} not found.")
         return publisher
+
+    @staticmethod
+    def options() -> List[Tuple[str, str]]:
+        publishers = PublisherService._get_all_publishers()
+        return [(str(p.publisher_id), p.legal_name) for p in publishers if not p.deleted]
