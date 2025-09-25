52e145a119334dda5d64d46c7dcd1d1ad814f4cd
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-09-25 09:40:54 -0300

    abstraindo regra de menu das views

--- view/view.py
+++ view/view.py
@@ -1,7 +1,10 @@
 import os
+from typing import Dict
 
 
 class View:
+
+    __INVALID_OPTION = 'Invalid option.'
 
     @staticmethod
     def clear_screen() -> None:
@@ -10,3 +13,13 @@ class View:
     @staticmethod
     def press_enter_to_continue() -> None:
         input('Press <Enter> to continue...')
+
+    @staticmethod
+    def run_action(menu_actions: Dict, option: str) -> bool:
+        action = menu_actions.get(option)
+        if not action:
+            print(View.__INVALID_OPTION)
+            View.press_enter_to_continue()
+            return True
+        result = action()
+        return result != 'exit'
