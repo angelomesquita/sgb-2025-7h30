3cf4d6231e22428a2550e5cf42a88ddffee16b00
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-09-25 09:48:49 -0300

    add validacao de nomes de autores duplicados

--- validators/author_validator.py
+++ validators/author_validator.py
@@ -1,3 +1,6 @@
+from typing import List
+
+from model.author import Author
 from validators.validator import Validator
 
 
@@ -10,3 +13,9 @@ class AuthorValidator:
     @staticmethod
     def validate_name(name: str) -> bool:
         return Validator.min_length(name, 3)
+
+    @staticmethod
+    def validate_unique_name(name: str, authors: List[Author]) -> bool:
+        normalized_name = name.strip().lower()
+        existing_names = set(a.name.strip().lower() for a in authors if not a.deleted)
+        return normalized_name not in existing_names
