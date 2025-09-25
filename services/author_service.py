3ef4efd2fb06d2d7caab7fa3644b4094685861fc
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-09-25 09:55:18 -0300

    tornando get_all_authors() publico

--- services/author_service.py
+++ services/author_service.py
@@ -7,13 +7,13 @@ from model.exceptions import AuthorNotFoundError
 class AuthorService:
 
     @staticmethod
-    def _get_all_authors() -> List[Author]:
+    def get_all_authors() -> List[Author]:
         """Load all authors from DAO (active and deleted)"""
         return AuthorDao.load_all()
 
     @staticmethod
     def get_author_by_id(author_id: str) -> Author:
-        authors = AuthorService._get_all_authors()
+        authors = AuthorService.get_all_authors()
         author = next((a for a in authors if str(a.author_id) == author_id), None)
         if author is None:
             raise AuthorNotFoundError(f"Author with id {author_id} not found.")
@@ -21,5 +21,5 @@ class AuthorService:
 
     @staticmethod
     def options() -> List[Tuple[str, str]]:
-        authors = AuthorService._get_all_authors()
+        authors = AuthorService.get_all_authors()
         return [(str(a.author_id), a.name) for a in authors if not a.deleted]
