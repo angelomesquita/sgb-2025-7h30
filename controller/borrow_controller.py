bb4eff0d95c320c128d33d13f3a7b2487f535b87
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-10-09 09:53:57 -0300

    refatorando borrow_controller que agora herda de base_controller

--- controller/borrow_controller.py
+++ controller/borrow_controller.py
@@ -1,52 +1,92 @@
 from datetime import date, timedelta
-from typing import List, Optional
+
+from controller.base_controller import BaseController
 from model.borrow import Borrow
 from model.borrow_dao import BorrowDao
 from model.logger import borrow_logger
-from model.exceptions import BorrowLoadError
+from model.exceptions import (
+    BookNotAvailableError,
+    BorrowAlreadyExistsError,
+    BorrowDeletedError,
+    BorrowNotFoundError,
+    BorrowLoadError
+)
 from repository.book_repository import BookRepository
 from repository.employee_repository import EmployeeRepository
 from repository.customer_repository import CustomerRepository
 
 
-class BorrowController:
+class BorrowController(BaseController[Borrow]):
+
+    _BORROW_DAYS: int = 7
+
+    dao_class = BorrowDao
+    logger = borrow_logger
+
+    AlreadyExistsError = BorrowAlreadyExistsError
+    DeleteExistsError = BorrowDeletedError
+    NotFoundError = BorrowNotFoundError
+    LoadError = BorrowLoadError
 
     def __init__(self):
-        self._borrow_days = 7
-        self.borrows: List[Borrow] = []
-        try:
-            self.borrows = BorrowDao.load_all()
-            borrow_logger.info(f"{self.__class__.__name__} loaded successfully.")
-        except BorrowLoadError as e:
-            borrow_logger.error(f"{self.__class__.__class__}: Failed to load borrows - {e}")
-
-    def find(self, borrow_id: str) -> Optional[Borrow]:
-        for borrow in self.borrows:
-            if borrow.borrow_id == borrow_id:
-                return borrow
-        return None
+        super().__init__(model_class=Borrow, key_field="borrow_id")
 
     def register(self, borrow_id: str, book_isbn: str, employee_cpf: str, customer_cpf: str) -> None:
-        if not borrow_id:
-            print('❌ borrow_id is required.')
-            return
-        if self.find(borrow_id):
-            print(f'An entry with this {borrow_id} is already registered!\n')
-            return
+        try:
+            BookRepository.decrease_quantity(book_isbn=book_isbn)
+            super().register(borrow_id, book_isbn=book_isbn, employee_cpf=employee_cpf, customer_cpf=customer_cpf)
+        except BookNotAvailableError as e:
+            self.logger.error(str(e))
+            print(f"⚠ Cannot register borrow: {e}")
 
-        borrow = Borrow(
-            borrow_id=borrow_id,
-            book=BookRepository.get_book_by_isbn(book_isbn),
-            employee=EmployeeRepository.get_employee_by_cpf(employee_cpf),
-            customer=CustomerRepository.get_customer_by_cpf(customer_cpf),
-            start_date=date.today(),
-            due_date=date.today() + timedelta(days=self._borrow_days),
-            return_date=None,
-            returned=False
+    def create_instance(
+        self,
+        borrow_id: str,
+        book_isbn: str,
+        employee_cpf: str,
+        customer_cpf: str,
+        return_date: date = None,
+        returned: bool = False,
+        deleted: bool = False
+    ) -> Borrow:
+
+        book = BookRepository.get_book_by_isbn(book_isbn)
+        employee = EmployeeRepository.get_employee_by_cpf(employee_cpf)
+        customer = CustomerRepository.get_customer_by_cpf(customer_cpf)
+        start_date = date.today()
+        due_date = start_date + timedelta(days=self._BORROW_DAYS)
+
+        return Borrow(
+            borrow_id,
+            book,
+            employee,
+            customer,
+            start_date,
+            due_date,
+            return_date,
+            returned,
+            deleted
         )
 
-        self.borrows.append(borrow)
-        BorrowDao.save_all(self.borrows)
-        message = '✅ Borrow successfully registered!'
-        borrow_logger.info(f"{message} [{borrow}]")
-        print(message)
+    def update(
+        self,
+        borrow_id: str,
+        book_isbn: str,
+        employee_cpf: str,
+        customer_cpf: str,
+        return_date: date = None,
+        returned: bool = False
+    ) -> None:
+
+        book = BookRepository.get_book_by_isbn(book_isbn)
+        employee = EmployeeRepository.get_employee_by_cpf(employee_cpf)
+        customer = CustomerRepository.get_customer_by_cpf(customer_cpf)
+
+        super().update(
+            borrow_id,
+            book=book,
+            employee=employee,
+            customer=customer,
+            return_date=return_date,
+            returned=returned
+        )
