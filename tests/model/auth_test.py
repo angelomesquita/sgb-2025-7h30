66793f73716128ef3bc01973b2f8ac79b7480b0c
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-11-13 10:41:54 -0300

    add fixtures e parametrize nos testes da model auth

--- tests/model/auth_test.py
+++ tests/model/auth_test.py
@@ -7,108 +7,118 @@ from unittest.mock import Mock, patch
 
 
 @pytest.fixture
-def credentials_data():
-    """Fixture that provides a valid password and hash for the tests."""
+def credentials():
+    """
+    Fixture that provides basic authentication credentials for the tests.
+
+    Returns:
+        dict: A dictionary containing:
+            - 'username' (str): valid username
+            - 'password' (str): plain text password
+            - 'hashed' (str): bcrypt hash generated from the password
+    """
+    username = 'admin'
     password = '123'
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-    return {'password': password, 'hashed': hashed}
+    return {'username': username, 'password': password, 'hashed': hashed}
 
 
-def test_hash_password_create_valid_hash(credentials_data):
-    """Ensure hash_password method returns a valid hash bcrypt and is not equal to the original password."""
-    hashed = Auth.hash_password(password=credentials_data['password'])
+def test_hash_password_create_valid_hash(credentials):
+    """
+    Ensure that the hash_password() method creates a valid bcrypt hash
+    that is not equal to the original password.
+
+    Scenario:
+        - Uses the password provided by the `credentials` fixture.
+        - Verifies that the generated hash is a string, different from
+          the plain password, and matches the original when validated
+          using bcrypt.
+    """
+    hashed = Auth.hash_password(password=credentials['password'])
 
     assert isinstance(hashed, str)
-    assert hashed != credentials_data['password']
-    assert bcrypt.checkpw(credentials_data['password'].encode('utf-8'), hashed.encode('utf-8'))
-
-# TODO: verify log register
+    assert hashed != credentials['password']
+    assert bcrypt.checkpw(credentials['password'].encode('utf-8'), hashed.encode('utf-8'))
 
 
-def test_hash_password_raises_exception(monkeypatch):
-    """Should raise an exception if bcrypt.hashpw fails."""
-    # noinspection PyUnusedLocal
-    def fake_hashpw(*args, **kwargs):
-        raise ValueError("bcrypt failed")
-
-    with patch("model.auth.bcrypt.hashpw", side_effect=fake_hashpw):
-        with pytest.raises(ValueError, match="bcrypt failed"):
-            password = "123"
-            Auth.hash_password(password=password)
+@pytest.mark.parametrize('side_effect, expected_exception, expected_message', [
+    (ValueError('bcrypt failed'), ValueError, 'bcrypt failed'),
+])
+def test_hash_password_raises_exception(side_effect, expected_exception, expected_message):
+    """Should raise an exception if bcrypt.hashpw() fails."""
+    with patch('model.auth.bcrypt.hashpw', side_effect=side_effect):
+        with pytest.raises(expected_exception, match=expected_message):
+            Auth.hash_password('123')
 
 
-def test_verify_password_returns_true_for_correct_password(credentials_data):
-    """Ensure verify_password returns True for a matching password."""
-    assert Auth.verify_password(credentials_data['password'], credentials_data['hashed']) is True
+@pytest.mark.parametrize('password, expected', [
+    ('123', True),
+    ('WrongPassword', False)
+])
+def test_verify_password(credentials, password, expected):
+    """
+    Verify that que verify_password() method behaves correctly for both
+    matching and non-matching passwords.
 
-# TODO: verify log register
+    Fixture:
+        - credentials: Provides the valid hash password.
+
+    Parameters (via parametrize):
+        - password (str): Input password to be checked.
+        - expected (bool): Expected result (True if match, False otherwise).
+    """
+    assert Auth.verify_password(password, credentials['hashed']) is expected
 
 
-def test_verify_password_returns_false_for_incorrect_password(credentials_data):
-    """Ensure verify_password returns False for a wrong password."""
-    assert Auth.verify_password("wrong_password", credentials_data['hashed']) is False
-
-
-def test_verify_password_raises_exception(monkeypatch):
+@pytest.mark.parametrize('side_effect, expected_exception, expected_message', [
+    (ValueError('bcrypt failed'), ValueError, 'bcrypt failed'),
+])
+def test_verify_password_raises_exception(credentials, side_effect, expected_exception, expected_message):
     """Should raise an exception if bcrypt.checkpw fails."""
-    # noinspection PyUnusedLocal
-    def fake_checkpw(*args, **kwargs):
-        raise ValueError("bcrypt failed")
-
-    with patch("model.auth.bcrypt.checkpw", side_effect=fake_checkpw):
-        with pytest.raises(ValueError, match="bcrypt failed"):
-            password = "123"
-            hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-            Auth.verify_password("wrong_password", hashed)
+    with patch('model.auth.bcrypt.checkpw', side_effect=side_effect):
+        with pytest.raises(expected_exception, match=expected_message):
+            Auth.verify_password('wrong_password', credentials['hashed'])
 
 
-def test_auth_returns_true_when_username_and_password_match(credentials_data):
-    """Ensure auth() return True when username and password are correct."""
-    expected_username = "admin"
+@pytest.mark.parametrize('username, password, expected', [
+    ('admin', '123', True),
+])
+def test_auth_returns_true_when_credentials_are_valid(credentials, username, password, expected):
+    """
+    Ensure that Auth.auth() returns True when valid credentiais are provided.
 
+    Fixture:
+        - credentials: Provides the valid hash password.
+
+    Parameters (via parametrize):
+        - username (str): Username provided for authentication.
+        - password (str): Password provided for authentication.
+        - expected (bool): Expected result (True for successful authentication).
+
+    Scenario:
+        - A mock employee object is created with valid credentials.
+        - Auth.auth() should return True, confirming successful authentication.
+    """
     employee = Mock()
-    employee.username = expected_username
-    employee.password_hash = credentials_data['hashed']
+    employee.username = credentials['username']
+    employee.password_hash = credentials['hashed']
 
-    assert Auth.auth(employee, expected_username, credentials_data['password']) is True
+    assert Auth.auth(employee, username, password) is expected
 
 
-def test_auth_raise_authentication_exception_error_when_username_is_wrong(credentials_data):
-    """Ensure auth() raise AuthenticationError exception when username does not match."""
-    expected_username = "admin"
-    wrong_username = 'wrong_username'
-
+@pytest.mark.parametrize('username, password', [
+    ('wrong_username', '123'),
+    ('admin', 'wrong_password'),
+])
+def test_auth_raises_authentication_error(credentials, username, password):
+    """
+    Ensure auth() raise AuthenticationError when credentials are invalid.
+    """
     employee = Mock()
-    employee.username = expected_username
-    employee.password_hash = credentials_data['hashed']
+    employee.username = credentials['username']
+    employee.password_hash = credentials['hashed']
 
     with pytest.raises(AuthenticationError) as exception_info:
-        Auth.auth(employee, wrong_username, credentials_data['hashed'])
-    assert f"Invalid credentials for username: {wrong_username}" in str(exception_info.value)
+        Auth.auth(employee, username, password)
 
-
-def test_auth_raises_authentication_exception_error_when_password_is_wrong(credentials_data):
-    """Ensure auth() raise AuthenticationError exception when password does not match."""
-    expected_username = "admin"
-    wrong_password = 'wrong_password'
-
-    employee = Mock()
-    employee.username = expected_username
-    employee.password_hash = credentials_data['hashed']
-
-    with pytest.raises(AuthenticationError) as exception_info:
-        Auth.auth(employee, expected_username, wrong_password)
-    assert f"Invalid credentials for username: {expected_username}" in str(exception_info.value)
-
-
-def test_auth_raises_unexpected_exception(monkeypatch, credentials_data):
-    """Should raise unexpected exception if something goes wrong."""
-    expected_username = "admin"
-
-    employee = Mock()
-    employee.username = expected_username
-    employee.password_hash = credentials_data['hashed']
-
-    with patch("model.auth.Auth.verify_password", side_effect=Exception("unexpected error")):
-        with pytest.raises(Exception, match="unexpected error"):
-            Auth.auth(employee, expected_username, credentials_data['hashed'])
+    assert f'Invalid credentials for username: {username}' in str(exception_info)
