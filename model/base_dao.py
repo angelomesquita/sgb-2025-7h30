7d3e1d9f93d0bf70ac036f55b6c9087c15e78c98
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-11-19 08:06:09 -0300

    add base_dao para uso com o sqlite

---
+++ model/base_dao.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+from typing import Generic, Iterable, Optional, TypeVar
+import sqlite3
+
+T = TypeVar('T')
+
+
+class BaseDao(ABC, Generic[T]):
+    _DB_NAME = 'library.db'
+
+    @classmethod
+    def _get_connection(cls) -> sqlite3.Connection:
+        connection = sqlite3.connect(cls._DB_NAME)
+        connection.row_factory = sqlite3.Row
+        return connection
+
+    @classmethod
+    @abstractmethod
+    def create_table(cls) -> None:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def save(cls, item: T) -> None:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def get_all(cls) -> Iterable[T]:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def get_by_id(cls, item_id: str) -> Optional[T]:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def delete(cls, item_id: str) -> None:
+        pass
