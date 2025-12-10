c483fbd7c2d586a7993fa0c02f4fa7d5ab8121cd
Angelo Mesquita <a.mesquita@coblue.com.br>
2025-12-10 08:07:51 -0300

    feat(book_dao): add método create_table

--- model/book_dao.py
+++ model/book_dao.py
@@ -1,4 +1,6 @@
-from model.base_dao_file import BaseDao
+from typing import Optional, Iterable
+
+from model.base_dao import BaseDao, T
 from model.book import Book
 from repository.author_repository import AuthorRepository
 from repository.publisher_repository import PublisherRepository
@@ -6,20 +8,38 @@ from repository.publisher_repository import PublisherRepository
 
 class BookDao(BaseDao[Book]):
 
-    _FILE_PATH = 'books.txt'
+    @classmethod
+    def create_table(cls) -> None:
+        with cls._get_connection() as connection:
+            connection.execute(
+                """
+                CREATE TABLE IF NOT EXISTS books (
+                    isbn TEXT PRIMARY KEY,
+                    title TEXT NOT NULL,
+                    author_id TEXT NOT NULL,
+                    publisher_id TEXT NOT NULL,
+                    year INTEGER NOT NULL,
+                    quantity TEXT NOT NULL,
+                    deleted INTEGER NOT NULL DEFAULT 0,
+                    FOREIGN KEY (author_id) REFERENCES authors(author_id),
+                    FOREIGN KEY (publisher_id) REFERENCES publishers(publisher_id)
+                )
+                """
+            )
+            connection.commit()
 
-    @staticmethod
-    def _serialize(b: Book) -> str:
-        return f"{b.isbn}|{b.title}|{b.author.author_id}|{b.publisher.publisher_id}|{b.year}|{b.quantity}|{b.deleted}"
+    @classmethod
+    def save(cls, item: T) -> None:
+        pass
 
-    @staticmethod
-    def _deserialize(data: str) -> Book:
-        isbn, title, author_id, publisher_id, year, quantity, deleted = data.split("|")
+    @classmethod
+    def get_all(cls) -> Iterable[T]:
+        pass
 
-        author = AuthorRepository.get_author_by_id(author_id)
-        publisher = PublisherRepository.get_publisher_by_id(publisher_id)
+    @classmethod
+    def get_by_id(cls, item_id: str, deleted: int = 0) -> Optional[T]:
+        pass
 
-        book = Book(isbn, title, author, publisher, year, quantity, deleted)
-        book.deleted = deleted.lower() == "true"
-
-        return book
+    @classmethod
+    def delete(cls, item_id: str) -> None:
+        pass
--- start_db.py
+++ start_db.py
@@ -1,4 +1,5 @@
 from model.author_dao import AuthorDao
+from model.book_dao import BookDao
 from model.publisher_dao import PublisherDao
 
 if __name__ == '__main__':
@@ -7,3 +8,6 @@ if __name__ == '__main__':
 
     PublisherDao.create_table()
     print('Publisher table created successfully.')
+
+    BookDao.create_table()
+    print('Book table created successfully.')
